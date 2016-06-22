eb79270388d8f532eebe7f9ad66161c8385a567c
Ashwin Ramesh <ashwinr64@gmail.com>
2016-06-22 06:21:16 +0530

    onyx: Add a camerawrapper
    * Taken from victara, modified for onyx.
    
    Change-Id: Ife828b6393a163ac946bf436661ac7aa89a2cff2

--- device.mk
+++ device.mk
@@ -108,6 +108,7 @@ PRODUCT_PACKAGES += \
 # Camera
 PRODUCT_PACKAGES += \
     android.hardware.camera.provider@2.4-service.onyx \
+    camera.msm8974 \
     libshims_atomic \
     libshims_camera
 
