e3d9182bb71265b891df2d30bdc2cdf798cd3f35
Ethan Chen <intervigil@gmail.com>
2016-10-30 12:44:42 +0200

    onyx: Add GraphicBuffer shim for camera
    * Needed by Old camera HAL
    
    Change-Id: Idb1860a05b3669581951ac303bdefbb8ff0c8a99

--- device.mk
+++ device.mk
@@ -108,7 +108,8 @@ PRODUCT_PACKAGES += \
 # Camera
 PRODUCT_PACKAGES += \
     android.hardware.camera.provider@2.4-service.onyx \
-    libshims_atomic
+    libshims_atomic \
+    libshims_camera
 
 # Bluetooth
 PRODUCT_PACKAGES += \
