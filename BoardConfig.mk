5da8db1c9dfb64f16231b6a5fcbf06a74d5fe310
CheckYourScreen <nimitmehta95@gmail.com>
2017-07-18 12:31:29 +0200

    onyx: Enable charger suspend
    Signed-off-by: CheckYourScreen <nimitmehta95@gmail.com>

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -94,6 +94,8 @@ USE_DEVICE_SPECIFIC_CAMERA := true
 
 # Charger
 BOARD_CHARGER_DISABLE_INIT_BLANK := true
+BOARD_CHARGER_ENABLE_SUSPEND := true
+BACKLIGHT_PATH := /sys/class/leds/lcd-backlight/brightness
 
 # Browser
 TARGET_USES_AOSP_BROWSER := true
