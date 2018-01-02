190d09cd1aa28b4e2805a6066745bae23b9afa48
YumeMichi <do4suki@gmail.com>
2018-01-02 14:09:11 +0800

    onyx: Add DRM binderized service
    Signed-off-by: YumeMichi <do4suki@gmail.com>

--- device.mk
+++ device.mk
@@ -103,7 +103,8 @@ PRODUCT_PACKAGES += \
 
 # DRM
 PRODUCT_PACKAGES += \
-    android.hardware.drm@1.0-impl
+    android.hardware.drm@1.0-impl \
+    android.hardware.drm@1.0-service
 
 # Camera
 PRODUCT_PACKAGES += \
