5a011565405629ebdf3219dbbac861ac8290de01
YumeMichi <do4suki@gmail.com>
2018-03-19 10:20:52 +0800

    onyx: Switch to OOS camera HAL
    Change-Id: I4b962c05ce0abe14b21785226fbaf041d64a76e6
    Signed-off-by: YumeMichi <do4suki@gmail.com>

--- device.mk
+++ device.mk
@@ -108,11 +108,7 @@ PRODUCT_PACKAGES += \
 # Camera
 PRODUCT_PACKAGES += \
     android.hardware.camera.provider@2.4-service.onyx \
-    camera.msm8974 \
-    libboringssl-compat \
-    libstlport \
-    libshims_atomic \
-    Snap
+    libshims_atomic
 
 # Bluetooth
 PRODUCT_PACKAGES += \
