c7ea4bf4db3325117d323bd0b659eba6c31e687e
Chippa-a <vusal1372@gmail.com>
2018-05-14 23:24:44 +0300

    onyx: Use custom healthd charger_res_images
     * Graphics are taken from stock Sony LP
    
    Change-Id: Ib08f16bfe350a4149a66373f7d9b53efd1e438fa

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -92,6 +92,7 @@ TARGET_NEEDS_PLATFORM_TEXT_RELOCATIONS := true
 # Charger
 BOARD_CHARGER_DISABLE_INIT_BLANK := true
 BOARD_CHARGER_ENABLE_SUSPEND := true
+BOARD_HEALTHD_CUSTOM_CHARGER_RES := $(PLATFORM_PATH)/charger/images
 BACKLIGHT_PATH := /sys/class/leds/lcd-backlight/brightness
 
 # CM Hardware
