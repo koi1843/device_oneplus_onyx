65f869c07075274ff56cc6eb6d99e5ad347be731
YumeMichi <do4suki@gmail.com>
2018-05-26 01:28:17 +0000

    onyx: Nuke legacy camera HIDL packages
    Signed-off-by: YumeMichi <do4suki@gmail.com>

--- hidl.mk
+++ hidl.mk
@@ -12,9 +12,9 @@ PRODUCT_PACKAGES += \
 
 # Camera
 PRODUCT_PACKAGES += \
-    android.hardware.camera.provider@2.4-impl-legacy \
+    android.hardware.camera.provider@2.4-impl \
     android.hardware.camera.provider@2.4-service.onyx \
-    camera.device@1.0-impl-legacy
+    camera.device@1.0-impl
 
 # Bluetooth
 PRODUCT_PACKAGES += \
