a6f4b6b2a0b7191a50b119516982d102c44ffcad
YumeMichi <do4suki@gmail.com>
2018-06-19 14:17:45 +0800

    Revert "onyx: Kang HWUI & Dalvik values from bacon"
    This reverts commit ddee668fbaacf9fe9ec2a7b9b53a5b5f5428098a.
    
    Signed-off-by: YumeMichi <do4suki@gmail.com>

--- device.mk
+++ device.mk
@@ -250,29 +250,6 @@ PRODUCT_PACKAGES += \
     wcnss_service \
     wificond
 
-# HWUI
-PRODUCT_PROPERTY_OVERRIDES += \
-    ro.hwui.texture_cache_size=72 \
-    ro.hwui.layer_cache_size=48 \
-    ro.hwui.path_cache_size=32 \
-    ro.hwui.gradient_cache_size=1 \
-    ro.hwui.drop_shadow_cache_size=6 \
-    ro.hwui.r_buffer_cache_size=8 \
-    ro.hwui.texture_cache_flushrate=0.4 \
-    ro.hwui.text_small_cache_width=1024 \
-    ro.hwui.text_small_cache_height=1024 \
-    ro.hwui.text_large_cache_width=2048 \
-    ro.hwui.text_large_cache_height=1024
-
-# Dalvik
-PRODUCT_PROPERTY_OVERRIDES += \
-    dalvik.vm.heapstartsize=16m \
-    dalvik.vm.heapgrowthlimit=192m \
-    dalvik.vm.heapsize=512m \
-    dalvik.vm.heaptargetutilization=0.75 \
-    dalvik.vm.heapminfree=2m \
-    dalvik.vm.heapmaxfree=8m
-
 # Vendor security patch level
 PRODUCT_PROPERTY_OVERRIDES += \
     ro.lineage.build.vendor_security_patch=2016-11-10
